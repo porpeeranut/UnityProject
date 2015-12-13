f6cebe91ae44569c062b5caafcb32e82f489fa6b
Patsawut Tananchai <oakpatsawut@gmail.com>
2015-12-13 18:18:49 +0700

    have player

---
+++ Assets/Scripts/Smoke.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Smoke : MonoBehaviour {
+
+	
+	float startTime;
+	float lifeTime;
+	float timer;
+	// Use this for initialization
+	void Start () {
+		lifeTime = 3.0f;
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time >= (startTime+lifeTime)){
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider obj){
+
+		if(obj.gameObject.CompareTag("Human")){
+				//obj.GetComponent<PlayerHealth>().AddHealth(2);
+}
+
+}
+
+}
