690966bdbd635ea513a08d13c9c402c3fe919317
Peeranat Thantaletong <porpeeranut@gmail.com>
2015-12-16 17:10:14 +0700

    fix bullet point to crosshair

--- Assets/Scripts/PlayerScripts/firstGun.cs
+++ Assets/Scripts/PlayerScripts/firstGun.cs
@@ -6,22 +6,17 @@ public class firstGun : MonoBehaviour {
 	public GameObject bullet;
 	public float initialSpeed;
 
-	
-	void Start () {	
-		
+	void Start () {
 		InvokeRepeating("Shooting",1.0f,0.1f);
 		initialSpeed = 50.0f;
-
 	}
-	
-	
-	void  Shooting(){
 
-		Vector3 tmpVec = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-		//tmpVec += transform.up * 0.5f;
-		GameObject gunBullet = null;
-		gunBullet = (GameObject)Instantiate(bullet, tmpVec, transform.rotation);
-		gunBullet.GetComponent<Rigidbody> ().velocity = transform.forward * initialSpeed;
+	void  Shooting(){
+		RaycastHit screenRayInfo;
+		if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Screen.width/2f, Screen.height/2f, 0)), out screenRayInfo, 500)) {
+			transform.LookAt(screenRayInfo.point);
+			GameObject gunBullet = (GameObject)Instantiate(bullet, transform.position, transform.rotation);
+			gunBullet.GetComponent<Rigidbody> ().velocity = transform.forward * initialSpeed;
+		}
 	}
 }
-
