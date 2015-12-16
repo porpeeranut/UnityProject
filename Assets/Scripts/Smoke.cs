aad31435290079e8db6f0dee2004d14de269d7cd
Patsawut Tananchai <oakpatsawut@gmail.com>
2015-12-16 20:48:35 +0700

    smoke

--- Assets/Scripts/Smoke.cs
+++ Assets/Scripts/Smoke.cs
@@ -7,23 +7,27 @@ public class Smoke : MonoBehaviour {
 	float startTime;
 	float lifeTime;
 	float timer;
+	float initialSpeed;
 	// Use this for initialization
 	void Start () {
-		lifeTime = 3.0f;
+		lifeTime = 5.0f;
 		startTime = Time.time;
+		initialSpeed = 3.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		GetComponent<Rigidbody>().velocity = transform.forward * initialSpeed;
 		if(Time.time >= (startTime+lifeTime)){
-			Destroy(gameObject);
+			//Destroy(gameObject);
 		}
 	}
 
 	void OnTriggerEnter(Collider obj){
 
 		if(obj.gameObject.CompareTag("Human")){
-				//obj.GetComponent<PlayerHealth>().AddHealth(2);
+
+			obj.GetComponent<humanTransform>().AddTp(2);
 }
 
 }
