ed1606a8d659467b10b0511eee34897b618012e1
Peeranat Thantaletong <porpeeranut@gmail.com>
2015-12-18 05:09:09 +0700

    add check game status, player victory anim

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStatus : MonoBehaviour {
+
+	void Update () {
+		if (GameObject.FindGameObjectWithTag ("Human") == null) {
+			GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool ("victory", true);
+			Invoke("restartGame", 10f);
+		}
+	}
+
+	void restartGame () {
+		Application.LoadLevel("Home");
+	}
+}
